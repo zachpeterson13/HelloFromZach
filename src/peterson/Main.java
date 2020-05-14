ed2f890b1d1e70346190609929db7a353d995b35
Guilherme Faccinetto <guilhermeffaccinetto@gmail.com>
2020-05-14 15:31:05 -0600

    Changes from Guilherme

--- src/peterson/Main.java
+++ src/peterson/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Zach.");
+		System.out.println("Hello Zach, nice to meet you. I'm Guilherme");
     }
 }
